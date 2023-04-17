3d48bdac727e2c1cdbd2626912235da8ace2c954
dansanch <danielalexsanchez@gmail.com>
2023-04-17 19:16:19 +0100

    Fix folder structure and update import statement

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
 
-const itemsRouter = require('./quest-items-showcase/routes/ItemRouter');
+const itemsRouter = require('./quest-items-showcase/routes/itemsRoutes');
 
 
 
